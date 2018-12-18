0c45e63f04e5349809701d2e58c6d864e6e97328
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-18 23:10:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,12 @@ require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
-<<<<<<< HEAD
+
   enable :sessions
   use Rack::Flash
 
 
-=======
->>>>>>> 48e8d83df038b7fc83a43e967aa0f74048926d76
+
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
